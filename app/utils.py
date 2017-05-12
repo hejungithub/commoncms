f70dbe0c3a0331f1ccce217d6af5f879d339036e
hejungithub <hejunemails@163.com>
2017-05-12 15:45:52 +0800

    cmo

--- app/utils.py
+++ app/utils.py
@@ -2,7 +2,7 @@ import logging
 import logging.config
 
 logging.config.fileConfig("logger.conf")
-logger = logging.getLogger("example02")
+logger = logging.getLogger("logfile")
 
 
 class Utils(object):
