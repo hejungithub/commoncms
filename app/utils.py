91a5e604e5f15656111669f93a8ea59d856f1329
hejungithub <hejunemails@163.com>
2017-05-12 16:27:45 +0800

    cmo

--- app/utils.py
+++ app/utils.py
@@ -1,8 +1,13 @@
 import logging
 import logging.config
 
-logging.config.fileConfig("logger.conf")
-logger = logging.getLogger("logfile")
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename='myapp.log',
+                    filemode='w')
+
+logger = logging.getLogger()
 
 
 class Utils(object):
